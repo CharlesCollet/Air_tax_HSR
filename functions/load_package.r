7e419f4fc96e597983ed8a2e51c716142799da6b
CharlesCollet <colletcharles@hotmail.fr>
2023-05-12 15:46:38 +0200

    automation of package installing

--- functions/load_package.r
+++ functions/load_package.r
@@ -1,3 +1,6 @@
+list.of.packages <- c(tidyverse,readr,tidyr,tidyverse,dplyr,stringr,xlsx,geosphere,cartograflow,R.utils,lmtest,plm,estimatr,ggplot2,lfe,data.table,fixest,did2s,reporttools)
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
 library(readr)
 library(tidyr)
 library(tidyverse)
@@ -17,5 +20,4 @@ library(data.table)
 library(fixest)
 library(did2s)
 library(reporttools)
-library(readxl)
 
